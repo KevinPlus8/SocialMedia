9788a3347d5cd1e9c20600cb41c88684feaa7be0
Kevin Eduardo Carrillo Oliva <kcarrillo@plusti.com>
2023-07-21 08:56:03 -0600

    first commit

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Infraestructure.Repositories;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPost() {
+            var post = new PostRepository().GetPosts();
+            return Ok(null);
+        }
+    }
+}
